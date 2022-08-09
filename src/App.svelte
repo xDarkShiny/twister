8ca70b3220d4af9458d4a524e041dfbb8c0fec89
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-08-09 13:07:45 +0300

    new

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+  import Navbar from "./lib/navbar.svelte";
+  import data from "./data.json";
+  import Carousel from 'svelte-carousel'
+ 
+
+  let carousel;
+  function goToNextPage() {
+    carousel.goToNext({ animated: true })
+  }
+  function goToPrevPage() {
+    carousel.goToPrev({ animated: true })
+  }
+  function page () {
+    return carousel.pagesCount
+  }
+</script>
+<style>
+  .text-font {
+      font-family: 'Inter', sans-serif;
+  }
+</style>
+<body class="bg-black">
+
+
+<Navbar />
+
+  <Carousel
+  bind:this={carousel}
+  arrows={false}
+  autoplay={true}
+  dots={false}
+  autoplayDuration={9000}
+  let:currentPageIndex
+  let:pagesCount
+  let:showPage
+  autoplayProgressVisible
+  pauseOnFocus
+>
+
+
+ 
+  {#each data as { largeImage, smallImage, season, description, name, puan, width, height}}
+  <div class="relative overflow-hidden rounded-lg shadow-lg cursor-pointer">
+    <div style="background-image: url({largeImage})" class="bg-cover min-h-screen bg-black"></div>
+    <div class="absolute inset-0 bg-black/50"></div>
+    <div class="absolute z-40 flex flex-col top-0 left-0 mt-52 2xl:ml-32 px-6 py-4">
+      <img class="" src={smallImage} width={width} height="" alt="">
+       <h1 class="text-font">{season}</h1>
+    </div>
+    </div>
+  {/each}
+  
+
+</Carousel>
+<div class="absolute flex gap-3 2xl:top-[54rem] xl:top-[44rem] lg:top-[44rem] md:top-[44rem] top-[44rem] right-5">
+  <button class="px-3 py-2 backdrop-blur-sm rounded-[6.5px] bg-black/50 text-white" on:click={goToPrevPage}><i class="text-2xl fa-solid fa-chevron-left"></i></button>
+  <button class="px-3 py-2 backdrop-blur-sm rounded-[6.5px] bg-black/50 text-white" on:click={goToNextPage}><i class="text-2xl fa-solid fa-chevron-right"></i></button>
+
+</div>
+
+</body>
---
+++ src/lib/navbar.svelte
@@ -0,0 +1,34 @@
+<style>
+    .text-font {
+        font-family: 'Inter', sans-serif;
+    }
+</style>
+<div class="w-full left-0 top-0 z-40 fixed px-5 py-3 bg-gradient-to-b">
+        <div class="w-full backdrop-blur-sm bg-black/50 rounded-2xl px-5 py-3  items-center justify-between ">
+            <div class="flex items-center justify-between">
+            <img class="h-8" src="https://media.discordapp.net/attachments/938854772570718238/985242585679413248/f3fcad5b273b3a1bf44f06db104b3109.png" alt="">
+            <div class="sm:block hidden">
+                <div class="flex  gap-5">
+                    <div class="text-font flex-col font-bold">
+                        <a href="aaga" class="text-font  text-lg text-white hover:text-cyan-500 outline-none focus:outline-none  ease-linear transition-all duration-150 font-bold">
+                            Anasayfa
+                        </a>
+                    </div>
+                    <div class="text-font flex-col font-bold">
+                        <a href="aaga" class="text-font text-lg text-white ml-1  hover:text-cyan-500 outline-none focus:outline-none  ease-linear transition-all duration-150 font-bold">
+                            Trendler
+                        </a>
+                    </div>
+                    <div class="text-font items-center flex-col font-bold">
+                        <a href="aaga" class="text-font text-lg text-white ml-2 hover:text-cyan-500 outline-none focus:outline-none  ease-linear transition-all duration-150 font-bold">
+                            Keşfet
+                        </a>
+                    </div>
+            </div>
+            </div> 
+            <a href="amk" class="bg-cyan-500 text-style hover:bg-cyan-700 text-white font-bold uppercase text-sm md:px-6 md:py-3 px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                    Oturum Aç 
+            </a>
+            </div> 
+        </div>
+</div>
