73804601f9303e52936156d1cdef860b9989a2a2
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-11-12 17:33:13 +0300

    yeni abi ya

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="fixed top-0 flex justify-between w-full bg-black/70 items-center text-white px-10 py-3">
+    <img class="h-8" src="https://pngimg.com/uploads/netflix/netflix_PNG4.png" alt="">
+    <div class="hidden sm:block">
+        <div class="flex  font-light text-lg transition-all duration-500 font-inter gap-3">
+            <p class="hover:bg-white/80 hover:text-black transition-all duration-500 px-4 py-1 rounded-lg ">Home</p>
+            <p class="hover:bg-white/80 hover:text-black transition-all duration-500 px-4 py-1 rounded-lg">Trend</p>
+            <p class="hover:bg-white/80 hover:text-black transition-all duration-500 px-4 py-1 rounded-lg">Discovery</p>
+        </div>    
+    </div>
+    <div class="hidden sm:block">
+        <div class="flex gap-3">
+            <button class="px-4 py-2  hover:bg-white/80 transition-all duration-250   bg-white text-black rounded-full font-inter">Login</button>
+            <button class="px-4 py-2  hover:bg-white/80 hover:text-black transition-all duration-250 f  ring-2 ring-white/80 text-white/80 rounded-full font-inter">Sing in</button>
+        </div>
+    </div>
+    <div class="block sm:hidden"><button class="px-4 py-2  hover:bg-white/80 transition-all duration-250   bg-white text-black rounded-full font-inter">Menu</button>    </div>
+</div>
+<div class="min-h-screen bg-black/70">
+
+</div>
